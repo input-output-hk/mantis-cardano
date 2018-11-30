590133ac67b3ab423a2f75e55f022324ce0fd972
Radek Tkaczyk <rtkaczyk@users.noreply.github.com>
2018-11-30 13:45:03 +0100

    GMC-179: rename txpool metrics

--- src/main/scala/io/iohk/ethereum/transactions/TxPoolMetrics.scala
+++ src/main/scala/io/iohk/ethereum/transactions/TxPoolMetrics.scala
@@ -8,11 +8,11 @@ class TxPoolMetrics(metrics: Metrics, txsRepeated: () => Double, maxProvisions: 
     * The number of transactions (currently kept) that were provided by the TX-pool more than once, could mean
     * that there was a problem executing those transaction (but could be other reasons, like block gas limit)
     */
-  final val TransactionsRepeated = metrics.gauge("tx-pool.txs-repeated", txsRepeated)
+  final val TransactionsRepeated = metrics.gauge("txpool.txs.repeated.number", txsRepeated)
 
   /**
     * The maximum number of repeats for a single transaction (currently kept in the pool)
     */
-  final val TransactionMaxProvisions = metrics.gauge("tx-pool.max-provisions", maxProvisions)
+  final val TransactionMaxProvisions = metrics.gauge("txpool.max.provisions.number", maxProvisions)
 
 }
